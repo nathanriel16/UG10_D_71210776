5943bb2ac6c2e3675286da300a9992008d584d00
Ariel <nathan.ariel@ti.ukdw.ac.id>
2021-11-19 07:13:29 +0700

    3_D_71210776

---
+++ 3_D_71210776.py
@@ -0,0 +1,10 @@
+#Soal 3
+daftar = str(input("Masukkan daftar belanja anda : "))
+print("Daftar Belanja : ", daftar.title().split())
+
+daftartambah = input("Masukkan barang yang ingin ditambahkan : ")
+hasil = daftar + " " + daftartambah.upper()
+if daftartambah in daftar :
+    print("Barang", daftartambah.upper(), "sudah berada dalam daftar belanja.")
+else :
+    print("Hasil penambahan pada daftar belanja : ", hasil.split())
